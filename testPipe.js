74301e43e347567751273d863db51fe7eb266b50
viksicom <github@viksi.com>
2018-02-23 08:50:34 -0500

    pipe testing

---
+++ testPipe.js
@@ -0,0 +1,25 @@
+const sf = require('./sanitize_files.js')
+const sanitize = sf.sanitize
+
+
+var options = {
+	patterns: [
+	{ 	regex: "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
+		token_name: "ipaddress"
+	},
+	{	regex: "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])\\.test\\.com",
+		token_name: "dnsaddress"
+	}],
+	tokenFile: "../../data_files/tokens.map",
+	reuseTokenFile: true,
+	whiteList: ["127.0.0.1","0.0.0.0"],
+	logger: {
+		format: {date:{show: false},type:{show: true}},
+		outputs: [
+			{file: "stdout",types: ["error"]},
+			{file: "pipe.log",types: ["error","info","stats","debug"]},
+		]
+	}
+}
+
+sanitize(options);
